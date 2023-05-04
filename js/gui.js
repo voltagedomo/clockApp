d4a76da7aba735954064827654fa0b32ec2737cf
voltagedomo <98976613+voltagedomo@users.noreply.github.com>
2023-05-04 14:47:09 -0600

    Add files via upload

---
+++ js/gui.js
@@ -0,0 +1,82 @@
+// Control GUI Changes
+
+// Variables
+let worldClockDiv = document.getElementById("world-clocks-div");
+let stopwatchDiv = document.getElementById("stopwatch-div");
+let timerDiv = document.getElementById("timer-div");
+let worldClocksLink = document.getElementById("selector-clocks");
+let stopwatchLink = document.getElementById("selector-stopwatch");
+let timerLink = document.getElementById("selector-timer");
+
+// Initialize div style and Selector style
+// switching div
+stopwatchDiv.style.display = "none";
+timerDiv.style.display = "none";
+worldClockDiv.style.display = "block";
+// altering style of selector to make it look better
+stopwatchLink.style.color = "blue";
+stopwatchLink.style.textDecoration = "underline";
+stopwatchLink.style.cursor = "pointer";
+timerLink.style.color = "blue";
+timerLink.style.textDecoration = "underline";
+timerLink.style.cursor = "pointer";
+worldClocksLink.style.color = "";
+worldClocksLink.style.textDecoration = "";
+worldClocksLink.style.cursor = "";
+
+// Stopwatch link clicked
+stopwatchLink.addEventListener("click", displayStopwatch);
+
+function displayStopwatch() {
+  // switching div
+  worldClockDiv.style.display = "none";
+  timerDiv.style.display = "none";
+  stopwatchDiv.style.display = "block";
+  // altering style of selector to make it look better
+  worldClocksLink.style.color = "blue";
+  worldClocksLink.style.cursor = "pointer";
+  timerLink.style.color = "blue";
+  timerLink.style.cursor = "pointer";
+  stopwatchLink.style.color = "";
+  stopwatchLink.style.textDecoration = "";
+  stopwatchLink.style.cursor = "";
+}
+// color: blue;
+// text-decoration: underline;
+// cursor: pointer;
+
+// Timer link clicked
+timerLink.addEventListener("click", displayTimer);
+
+function displayTimer() {
+  // switching div
+  worldClockDiv.style.display = "none";
+  stopwatchDiv.style.display = "none";
+  timerDiv.style.display = "block";
+  // altering style of selector to make it look better
+  stopwatchLink.style.color = "blue";
+  stopwatchLink.style.cursor = "pointer";
+  worldClocksLink.style.color = "blue";
+  worldClocksLink.style.cursor = "pointer";
+  timerLink.style.color = "";
+  timerLink.style.textDecoration = "";
+  timerLink.style.cursor = "";
+}
+
+// World clocks link clicked
+worldClocksLink.addEventListener("click", displayWorldClock);
+
+function displayWorldClock() {
+  // switching div
+  stopwatchDiv.style.display = "none";
+  timerDiv.style.display = "none";
+  worldClockDiv.style.display = "block";
+  // altering style of selector to make it look better
+  stopwatchLink.style.color = "blue";
+  stopwatchLink.style.cursor = "pointer";
+  timerLink.style.color = "blue";
+  timerLink.style.cursor = "pointer";
+  worldClocksLink.style.color = "";
+  worldClocksLink.style.textDecoration = "";
+  worldClocksLink.style.cursor = "";
+}
---
+++ js/stopwatch.js
@@ -0,0 +1,12 @@
+// Stopwatch part by Alexander Alkin - seperate js to reduce amount of scrolling needed
+
+// Variables
+let recordedTime = 0;
+let stoppedTime = 0;
+let laps = 0;
+startButton = document.getElementById("stopwatch-start");
+
+// event listeners
+startButton.addEventListener("click", stopwatch);
+
+function stopwatch(startTime, lap) {}
